4749ae6fd43402c3cce35e1498bf7b6f510b35a3
Aude Sedillo <aude.sedillo@gmail.com>
2024-06-11 17:17:36 +0200

    create navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import "./reset.css";
 
 import "./global.css";
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <main className="container">
+      <NavBar />
       <Outlet />
     </main>
   );
