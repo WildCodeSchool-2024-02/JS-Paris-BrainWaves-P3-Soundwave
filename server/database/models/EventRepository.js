4575f97d23cc7fa70afa535ae6a6e2124683481b
YannLahellec <lahellec.yann@gmail.com>
2024-06-11 16:21:55 +0200

    readOne done

--- server/database/models/EventRepository.js
+++ server/database/models/EventRepository.js
@@ -3,7 +3,16 @@ const AbstractRepository = require("./AbstractRepository");
 class EventRepository extends AbstractRepository {
     constructor() {
         super ({table : 'event'});
-    }
+        }
+        
+        // async create () {
+        //     const [results] = await this.database.query(
+        //         `INSERT INTO ${this.table} (name) VALUES`
+        //     )
+        //     return results
+        // }
+
+
 }
 
 module.exports = EventRepository;
