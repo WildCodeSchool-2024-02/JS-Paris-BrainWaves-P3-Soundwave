1c70784deaf20b97464c06c96aba2ff7e9ff6400
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-06-12 14:30:33 +0200

    crew list backend set

---
+++ server/app/routers/api/crews/router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+const { browse, read } = require("../../../controllers/crewAction");
+
+router.get("/", browse);
+router.get("/:id", read);
+
+module.exports = router;
