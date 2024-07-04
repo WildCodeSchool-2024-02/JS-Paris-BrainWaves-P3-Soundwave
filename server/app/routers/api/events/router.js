0397ba61af1ed82849f5a7cf22d8905a68a5d416
YannLahellec <lahellec.yann@gmail.com>
2024-07-04 16:06:45 +0200

    dead code erased

--- server/app/routers/api/events/router.js
+++ server/app/routers/api/events/router.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const {browse, read, editStatus, readPendingEvents, readCategoryEvents, readCrewByEvent} = require("../../../controllers/eventAction")
-// const { ValidateForm } = require("../../../services/validateEventForm")
+const {
+  browse,
+  read,
+  editStatus,
+  readPendingEvents,
+  readCategoryEvents,
+  readCrewByEvent,
+} = require("../../../controllers/eventAction");
 
 router.get("/", browse);
 router.get("/tovalidate", readPendingEvents);
@@ -11,6 +17,5 @@ router.get("/category/:style", readCategoryEvents);
 router.get("/:id/crew", readCrewByEvent);
 router.get("/:id", read);
 router.put("/:id", editStatus);
-// router.post("/", ValidateForm, add)
 
 module.exports = router;
