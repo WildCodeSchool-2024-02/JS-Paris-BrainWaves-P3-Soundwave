c78c6c79b503b560780169203bcad4c6dfd0ffa5
Aude Sedillo <aude.sedillo@gmail.com>
2024-07-12 15:26:01 +0200

    create user account + edit profile finish

--- server/app/routers/api/users/router.js
+++ server/app/routers/api/users/router.js
@@ -12,15 +12,16 @@ const {
 } = require("../../../controllers/userActions");
 
 const { ValidateUserForm } = require("../../../services/validateUserAccount");
+const { isAuth, isClient } = require("../../../services/auth");
+const imageUpload = require("../../../services/imageUpload")
 
 router.get("/", browse);
-router.post("/", add);
+router.post("/", ValidateUserForm, add);
 router.get("/refresh", refresh);
 router.post("/login", readLogin);
 router.get("/logout", logout);
-router.post("/", ValidateUserForm, add);
 
 router.get("/:id", read);
-router.put("/:id", edit);
+router.put("/",isAuth, isClient, imageUpload.single("image"), edit);
 
 module.exports = router;
